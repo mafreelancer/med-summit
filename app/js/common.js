e03f4aaa79a7231057b78a196fc49a3d97d94001
mafreelancer <mafri@yandex.ru>
2018-05-21 23:30:47 +0300

    Add JS-script for create simple animation of move the main menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,7 @@
+var button_open_menu = document.querySelector(".button__icon"),
+    aside = document.querySelector(".aside");
 
+button_open_menu.onclick = function() {
+    aside.classList.toggle("aside--show");
+    button_open_menu.classList.toggle("button__icon--open");
+}
